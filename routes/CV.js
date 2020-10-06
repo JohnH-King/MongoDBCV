fb442503a137c89d4e076f00dbac88bcf71a9c39
John King <Johnk858@ymail.com>
2020-10-06 21:59:01 +0200

    fix

--- routes/CV.js
+++ routes/CV.js
@@ -27,7 +27,6 @@ router.post('/', (req, res, next) => {
     console.log(result);
   })
   .catch(err => console.log(err));
-  alert('Thank you for your feedback!');
     res.status(201).json({
         message: 'Handling POST requests to /CV',
         createdItem: cvitem
